06467850e179ea6415d4a27675cbff96d5013fc4
Pawel Olejnik <olejnik.pablo@gmail.com>
2022-12-08 17:59:53 +0100

    Initial Commit

---
+++ Git project first/Git_project_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_project_firstApp.swift
+//  Git project first
+//
+//  Created by Pawel Olejnik on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Git_project_firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
